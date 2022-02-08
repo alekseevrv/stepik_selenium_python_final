2578a84e2757633dc7e55f3819db01675c43dd16
Alekseev Ruslan <alekseev.ruslan_@mail.ru>
2022-02-08 21:52:46 +0700

    Реализация, проверка LoginPage + переходы между страницами

--- test_main_page.py
+++ test_main_page.py
@@ -7,6 +7,7 @@ pytest -s -v --tb=line test_main_page.py
 """
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -14,4 +15,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
     page.should_be_login_link()  # проверка, что есть ссылка, которая ведет на логин
